9ca335088be7af03af00e9f84bc557bfb47799bd
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-08-13 23:00:55 -0500

    2020-08-11

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Counter Testing", () => {
+  test("render the title of counter", () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText('This is counter app');
+    expect(linkElement).toBeInTheDocument();
+  });
 });
