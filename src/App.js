38c7f01546703948f2d244603f1a8ba7e31313d1
ilhaifa@yandex.ru <ilhaifa@yandex.ru>
2020-08-14 21:58:32 -0500

    20-08-14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Counter from "./Counter";
 
 function App() {
+  
   return (
     <div className="App">
-      <h1>This is counter app</h1>
-
+      <Counter />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,45 @@
+//npm run test
 import React from "react";
-import { render } from "@testing-library/react";
+import { shallow, mount } from "enzyme";
 import App from "./App";
+import Counter from "./Counter";
 
 describe("Counter Testing", () => {
+  let wrapper;
+  beforeEach(() => {
+    //wrapper = shallow(<Counter />);
+    wrapper = mount(<App />);
+
+  });
+
   test("render the title of counter", () => {
-    const { getByText } = render(<App />);
-    const linkElement = getByText('This is counter app');
-    expect(linkElement).toBeInTheDocument();
+    //console.log(wrapper.debug());
+    expect(wrapper.find("h1").text()).toContain("This is counter app");
+
+    // const { getByText } = render(<App />);
+    // const linkElement = getByText("This is counter app");
+    // expect(linkElement).toBeInTheDocument();
+  });
+
+  test("render a button with text of increment", () => {
+    //const wrapper = shallow(<App />);
+    expect(wrapper.find("#increment-btn").text()).toBe("Increment");
+  });
+
+  test("render the initial value of state in a div", () => {
+    //const wrapper = shallow(<App />);
+    expect(wrapper.find("#counter-value").text()).toBe("0");
+  });
+
+  test("render the click event of increment button and increment counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+  });
+
+  test("render the click event of decrement button and deccrement counter value", () => {
+    wrapper.find("#increment-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("1");
+    wrapper.find("#decrement-btn").simulate("click");
+    expect(wrapper.find("#counter-value").text()).toBe("0");
   });
 });
